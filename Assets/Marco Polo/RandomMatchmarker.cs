d5c337be7d9fc98b4c06e38be0eef43a04790329
gageracer <gageracer@gmail.com>
2015-12-02 00:34:38 +0200

    Who Project Yet

---
+++ Assets/Marco Polo/RandomMatchmarker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMatchmarker : Photon.PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		PhotonNetwork.ConnectUsingSettings ("0.1");
+	
+	}
+
+	public override void OnJoinedLobby ()
+	{
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonRandomJoinFailed(){
+	
+		Debug.Log ("Can't join random room!");
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom(){
+	
+		GameObject monster = PhotonNetwork.Instantiate ("monsterprefab", Vector3.zero, Quaternion.identity, 0);
+		monster.GetComponent<myThirdPersonController>().isControllable = true;
+	}
+
+	void OnGUI(){
+
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
